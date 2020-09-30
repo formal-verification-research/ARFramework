70ddc1e51730ead977c75ddc05ea1ac3ed84dea8
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2020-09-30 13:31:38 -0600

    finished results for iclr

--- mnist/read_proto.py
+++ mnist/read_proto.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-from tensorflow.python.framework import graph_util
-from tensorflow.python.framework import graph_io
 
-with open('2020_01_22_19_08_26_4975_1_4.pb', 'rb') as f:
+with open('mnist_200.pb', 'rb') as f:
     a = tf.parse_tensor(f.read(), tf.float32)
 
 with tf.Session() as sess:
